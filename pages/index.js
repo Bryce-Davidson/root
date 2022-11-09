f78eb670b40520846a9017126c6797ae76930759
Bryce-Davidson <lifebryce@gmail.com>
2022-11-08 23:38:37 -0800

    Added dummy static props route

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,16 @@
-export default function Home() {
-  return <h1>Hello World!</h1>;
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return { props: { users: data } };
+};
+
+export default function Home({ users }) {
+  return (
+    <div>
+      {users.map((user) => {
+        return <p>{JSON.stringify(user)}</p>;
+      })}
+    </div>
+  );
 }
