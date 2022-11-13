822b3135716ecf38c77676397c53f18eece0d297
Bryce-Davidson <lifebryce@gmail.com>
2022-11-12 22:41:23 -0800

    Added dummy next.js route

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  fetch("https://mockend.com/Bryce-Davidson/geo-mock-api/points", {})
+    .then((response) => response.json())
+    .then((json) => res.status(200).json(json));
 }
