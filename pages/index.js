796d84eb834aa483701ac284acfecfc17a34a60d
Bryce-Davidson <lifebryce@gmail.com>
2022-11-12 19:20:31 -0800

    Added server code

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,12 @@
+export async function getServerSideProps(context) {
+  fetch("https://jsonplaceholder.typicode.com/todos/1")
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+  return {
+    props: {}, // will be passed to the page component as props
+  };
+}
+
 export default function Home() {
   return <h1 className="text-3xl font-bold">Hello world!</h1>;
 }
