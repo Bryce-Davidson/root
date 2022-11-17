a35ebd9f96585ccd7b958ddfed39efee0f5a1d02
Kelly Lemaire <kellythlemaire@uvic.ca>
2022-11-17 15:36:26 -0800

    Test commit

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import Head from "next/head";
 import Map, { Marker } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 
+cosnt test = ""
+
 const MAPBOX_TOKEN =
   "pk.eyJ1IjoiYnJ5Y3ljbGUiLCJhIjoiY2t5emhpcDZ1MG0yeDJ2bHV5Ymo3bzk4ZCJ9.K8F0fGovviZwfOYLOnbKhg";
 
