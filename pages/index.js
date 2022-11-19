77e3e4ecb201819871c9f48d6d3b3a0f6b89a473
Bryce-Davidson <lifebryce@gmail.com>
2022-11-18 19:31:01 -0800

    Added route on viewer

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,37 @@
 import * as React from "react";
-// import Head from "next/head";
-import Map, { Marker, FullscreenControl } from "react-map-gl";
+import fsPromises from "fs/promises";
+import path from "path";
+import Map, { Marker, FullscreenControl, Source, Layer } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
-
-const test = ""
 
 const MAPBOX_TOKEN =
   "pk.eyJ1IjoiYnJ5Y3ljbGUiLCJhIjoiY2t5emhpcDZ1MG0yeDJ2bHV5Ymo3bzk4ZCJ9.K8F0fGovviZwfOYLOnbKhg";
 
-export default function Home() {
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "/data/matched.json");
+  const jsonData = await fsPromises.readFile(filePath);
+  const objectData = JSON.parse(jsonData);
+
+  return {
+    props: { json: objectData },
+  };
+}
+
+const layerStyle = {
+  id: "route",
+  type: "line",
+  source: "route",
+  layout: {
+    "line-join": "round",
+    "line-cap": "round",
+  },
+  paint: {
+    "line-color": "#FFA500",
+    "line-width": 3,
+  },
+};
+
+export default function Home({ json }) {
   return (
     <div>
       <Map
@@ -23,6 +46,9 @@ export default function Home() {
       >
         <Marker longitude={-123.3327} latitude={48.427589} color="red" />
         <FullscreenControl />
+        <Source id="my-data" type="geojson" data={json}>
+          <Layer {...layerStyle} />
+        </Source>
       </Map>
     </div>
   );
