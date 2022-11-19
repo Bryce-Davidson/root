1c23fe70801c1acd58c0c586226a42d8d63596e8
Bryce-Davidson <lifebryce@gmail.com>
2022-11-18 19:49:42 -0800

    Added map matching API calls to index

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,59 @@
 import * as React from "react";
-import fsPromises from "fs/promises";
-import path from "path";
 import Map, { Marker, FullscreenControl, Source, Layer } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
-
+const matchClient = require("@mapbox/mapbox-sdk/services/map-matching.js");
 const MAPBOX_TOKEN =
   "pk.eyJ1IjoiYnJ5Y3ljbGUiLCJhIjoiY2t5emhpcDZ1MG0yeDJ2bHV5Ymo3bzk4ZCJ9.K8F0fGovviZwfOYLOnbKhg";
+const match = matchClient({ accessToken: MAPBOX_TOKEN });
+
+function createFeature(mbxGeometry) {
+  return {
+    type: "FeatureCollection",
+    features: [
+      {
+        type: "Feature",
+        properties: {},
+        geometry: mbxGeometry,
+      },
+    ],
+  };
+}
+
+function createWaypoints(sample) {
+  return sample.map((coord) => {
+    let lon = coord[0];
+    let lat = coord[1];
+    return {
+      coordinates: [lon, lat],
+    };
+  });
+}
+
+const routes = {
+  waterFront: [
+    [-123.34965713493622, 48.40585943690391],
+    [-123.33156366734758, 48.40875864411282],
+    [-123.31840950843966, 48.409034750465594],
+    [-123.30021179748223, 48.41310714933587],
+    [-123.30008080999716, 48.42128085339439],
+    [-123.3026636224426, 48.4375310516682],
+    [-123.2942287657707, 48.446220730732506],
+  ],
+};
 
 export async function getStaticProps() {
-  const filePath = path.join(process.cwd(), "/data/matched.json");
-  const jsonData = await fsPromises.readFile(filePath);
-  const objectData = JSON.parse(jsonData);
+  const res = await match
+    .getMatch({
+      tidy: true,
+      geometries: "geojson",
+      overview: "full",
+      points: createWaypoints(routes.waterFront),
+    })
+    .send();
 
+  const geo = res.body.matchings[0].geometry;
   return {
-    props: { json: objectData },
+    props: { json: createFeature(geo) },
   };
 }
 
